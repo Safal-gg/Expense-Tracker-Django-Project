aed003403ae38e1fdee37d513914b0fc67cc259f
Safal-gg <safal.shrestha355@gmail.com>
2025-12-02 21:44:32 +0545

    started the project by creating basic building blocks

---
+++ exp_trc/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ExpTrcConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'exp_trc'
---
+++ exp_trc/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Expense(models.Model):
+    """A topic the user is learning about."""
+    name=models.CharField(max_length=10)
+    amount=models.DecimalField(max_digits=5,decimal_places=2)
+    date_added=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
